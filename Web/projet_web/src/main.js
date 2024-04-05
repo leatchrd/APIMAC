127e1cf4ca93abceef85e595eabc4b28db45123c
leatchrd <lea.touchard@edu.esiee.fr>
2024-04-05 22:17:06 +0200

    Delete old project

--- Web/projet_web/src/main.js
+++
@@ -1,8 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import { getRatpData } from '@/service/api.js'
-
-createApp(App).mount('#app')
--- Web/projet_web/src/test.vue
+++
@@ -1,8 +0,0 @@
-<template>
-<h1> Nous sommes lundi !
-</h1>
-</template>
-
-<script>
-export default {}
-</script>
