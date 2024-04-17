016adfce664300e26289bdecb627b74dc104edd1
Léa <lea.touchard@edu.esiee.fr>
2024-04-17 19:31:17 +0200

    Footer ok normalement

---
+++ Web/API/src/components/Footer.vue
@@ -0,0 +1,18 @@
+// EN COURS
+
+<template>
+  <footer>
+      <p>By Léa TOUCHARD - IMAC2 2024</p>
+  </footer>
+</template>
+
+<style scoped>
+  footer {
+    display: flexbox;
+    height: 100px;
+    background-color: white;
+    padding-top: 40px;
+    justify-content: space-around;
+    text-align: center;
+  }
+</style>
