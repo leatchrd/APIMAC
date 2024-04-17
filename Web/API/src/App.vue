697ea7b577075dba28870f48eb062f155dd05951
Léa <lea.touchard@edu.esiee.fr>
2024-04-17 18:46:14 +0200

    Mouais

--- Web/API/src/App.vue
+++ Web/API/src/App.vue
@@ -1,76 +1,37 @@
-
 <template>
-  <div class="app">
-    <Header @search="searchCharacter" />
-    <Background :characters="displayedCharacters" />
+  <div class="dogs-gallery">
+    <DogCard :firstname="'Pluto'" :breed="'Saint-Hubert'" :pictureUrl="'https://my-best-kennel.com/pictures/45.png'"/>
+    <DogCard :firstname="'Rex'" :breed="'Labrador'" :pictureUrl="'https://my-best-kennel.com/pictures/46.png'"/>
   </div>
 </template>
-<script>
 
-import Header from "./components/Header.vue";
-import Background from "./components/Background.vue";
+<script>
+import getDogsData from "./components/dogsRepository.js";
 
 export default {
-  components: {
-    Header,
-    Background,
+  name: 'DogsGallery',
+  props: {
+    firstname: { type: String, required: true },
+    breed: { type: String, required: true },
+    pictureUrl: { type: String, default: "https://my-best-kennel.com/pictures/default.png" }
   },
-  data() {
-    return {
-      characters: [],
-      displayedCharacters: [],
-    };
-  },
-  methods: {
-    searchCharacter(searchQuery) {
-      // Filtrer les personnages correspondants à la recherche
-      this.displayedCharacters = this.characters.filter((character) => {
-        return character.name.toLowerCase().includes(searchQuery.toLowerCase());
-      });
-    },
-    handleSearchResults(results) {
-      this.displayedCharacters = results;
-    },
-  },
-  mounted() {
-    const storedCharacters = localStorage.getItem("characters");
-    if (storedCharacters) {
-      this.characters = JSON.parse(storedCharacters);
-      this.displayedCharacters = this.characters.slice(0, 18);
-    } else {
-      this.fetchCharacters();
+
+  data(){
+    return{
+        dogsData: []
     }
   },
+
   methods: {
-    fetchCharacters() {
-      fetch("https://api.disneyapi.dev/character")
-        .then((response) => response.json())
-        .then((data) => {
-          if (data && Array.isArray(data.data)) {
-            this.characters = data.data;
-            localStorage.setItem("characters", JSON.stringify(this.characters));
-          } else {
-            console.error("Invalid response data format");
-          }
-        })
-        .catch((error) => console.error("Error fetching characters", error));
-    },
-    shuffle(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-      }
-      return array;
-    },
-  },
-};
+    async retrieveDogsData(){
+        const myData = await getDogsData()
+    }
+  }
+}
 </script>
 
 <style>
-.app {
-  text-align: center;
-  background: linear-gradient(to bottom, #83d6c3, #7cacfb);
-  min-height: 100vh;
+.dogs-gallery {
+  display: flex;
 }
 </style>
-
---
+++ Web/API/src/components/dogsRepository.js
@@ -0,0 +1,17 @@
+function getDogsData() {
+    return [
+      {
+        "id": 45,
+        "name": "Pluto",
+        "breed": "Saint-Hubert",
+        "picture": "https://my-best-kennel.com/pictures/45.png",
+      },
+      {
+        "id": 46,
+        "name": "Rex",
+        "breed": "Labrador",
+        "picture": "https://my-best-kennel.com/pictures/46.png",
+      }
+    ];
+  }
+  
