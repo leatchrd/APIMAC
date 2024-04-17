f12722698b888bb8501c4594a69cc6199e87fedf
Léa <lea.touchard@edu.esiee.fr>
2024-04-17 18:47:14 +0200

    Màj récupération des données

--- Web/API/src/App.vue
+++ Web/API/src/App.vue
@@ -1,37 +1,75 @@
+
 <template>
-  <div class="dogs-gallery">
-    <DogCard :firstname="'Pluto'" :breed="'Saint-Hubert'" :pictureUrl="'https://my-best-kennel.com/pictures/45.png'"/>
-    <DogCard :firstname="'Rex'" :breed="'Labrador'" :pictureUrl="'https://my-best-kennel.com/pictures/46.png'"/>
+  <div class="app">
+    <Header @search="searchCharacter" />
+    <Background :characters="displayedCharacters" />
   </div>
 </template>
-
 <script>
-import getDogsData from "./components/dogsRepository.js";
+import Header from "./components/Header.vue";
+import Background from "./components/Background.vue";
 
 export default {
-  name: 'DogsGallery',
-  props: {
-    firstname: { type: String, required: true },
-    breed: { type: String, required: true },
-    pictureUrl: { type: String, default: "https://my-best-kennel.com/pictures/default.png" }
+  components: {
+    Header,
+    Background,
   },
-
-  data(){
-    return{
-        dogsData: []
-    }
+  data() {
+    return {
+      characters: [],
+      displayedCharacters: [],
+    };
   },
-
   methods: {
-    async retrieveDogsData(){
-        const myData = await getDogsData()
+    searchCharacter(searchQuery) {
+      // Filtrer les personnages correspondants à la recherche
+      this.displayedCharacters = this.characters.filter((character) => {
+        return character.name.toLowerCase().includes(searchQuery.toLowerCase());
+      });
+    },
+    handleSearchResults(results) {
+      this.displayedCharacters = results;
+    },
+  },
+  mounted() {
+    const storedCharacters = localStorage.getItem("characters");
+    if (storedCharacters) {
+      this.characters = JSON.parse(storedCharacters);
+      this.displayedCharacters = this.characters.slice(5, 20);
+      // this.displayedCharacters = this.characters.filter(character => character.name == "Achilles"); // On obtient le résultat Achilles
+    } else {
+      this.fetchCharacters();
     }
-  }
-}
+  },
+  methods: {
+    shuffle(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    },
+    mounted() {
+      fetch("http://api.disneyapi.dev/character?page=2&pageSize=100")
+        .then((response) => response.json())
+        .then((data) => {
+          if (data && Array.isArray(data.data)) {
+            this.characters = data.data.slice(0, 20);
+          } else {
+            console.error("Invalid response data format");
+          }
+        })
+        .catch((error) => console.error("Error fetching characters", error));
+    },
+  },
+};
 </script>
 
 <style>
-.dogs-gallery {
-  display: flex;
+.app {
+  text-align: center;
+  background: linear-gradient(to bottom, #83d6c3, #7cacfb);
+  min-height: 100vh;
 }
 </style>
+
