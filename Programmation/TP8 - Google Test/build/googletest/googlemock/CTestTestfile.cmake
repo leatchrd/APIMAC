b286a23c6970b1cf70a724b58746740072ac8bac
leatchrd <150906889+leatchrd@users.noreply.github.com>
2024-04-19 16:56:32 +0200

    Delete Programmation directory

--- Programmation/TP8 - Google Test/build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/6ima2/lea.touchard/Bureau/IMAC2/Programmation/TP8 - Google Test/googletest/googlemock
-# Build directory: /home/6ima2/lea.touchard/Bureau/IMAC2/Programmation/TP8 - Google Test/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
